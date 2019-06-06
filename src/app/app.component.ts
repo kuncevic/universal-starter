e82d6ea3a2a1f645d69e397ad7b4a44bf0547529
kuncevic <kncvch@gmail.com>
2019-06-06 17:47:57 +1000

    add gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,57 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare var gtag;
 
 @Component({
   selector: 'app-root',
   template: `
-  <div class="app-container">
-    <h1>Angular Universal Demo utilizing Angular & Angular CLI</h1>
-    <nav class="nav-links">
-      <a routerLink="/">Home</a>
-      <a routerLink="/lazy">Lazy-loaded Route</a>
-      <a routerLink="/lazy/nested">Nested Routes work too</a>
-    </nav>
-    <div class="router-container">
-      <router-outlet></router-outlet>
+    <div class="app-container">
+      <h1>Angular Universal Demo utilizing Angular & Angular CLI</h1>
+      <nav class="nav-links">
+        <a routerLink="/">Home</a>
+        <a routerLink="/lazy">Lazy-loaded Route</a>
+        <a routerLink="/lazy/nested">Nested Routes work too</a>
+      </nav>
+      <div class="router-container">
+        <router-outlet></router-outlet>
+      </div>
     </div>
-  </div>
   `,
-  styles: [`
-    :host {
-      background: #f1f1f1;
-      font-family: Roboto,"Helvetica Neue Light","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif;
-      font-display: swap;
-    }
-    .nav-links {
-      background: #008591;
-    }
-    .nav-links a {
-      color: #fff;
-      display: inline-block;
-      padding: 1rem;
-      margin-right: 3rem;
-      text-decoration: none;
-      font-weight: bold;
-      letter-spacing: 0.1rem;
-    }
-    .router-container {
-      border: 0.5rem #00afc4 solid;
-      padding: 2rem;
-    }
-  `]
+  styles: [
+    `
+      :host {
+        background: #f1f1f1;
+        font-family: Roboto, 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial,
+          'Lucida Grande', sans-serif;
+        font-display: swap;
+      }
+      .nav-links {
+        background: #008591;
+      }
+      .nav-links a {
+        color: #fff;
+        display: inline-block;
+        padding: 1rem;
+        margin-right: 3rem;
+        text-decoration: none;
+        font-weight: bold;
+        letter-spacing: 0.1rem;
+      }
+      .router-container {
+        border: 0.5rem #00afc4 solid;
+        padding: 2rem;
+      }
+    `
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(public router: Router) {}
 
+  ngOnInit() {
+    const navEndEvent$ = this.router.events.pipe(filter(e => e instanceof NavigationEnd));
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      gtag('config', 'UA-xxxxxxxxx-x', { page_path: e.urlAfterRedirects });
+    });
+  }
 }
